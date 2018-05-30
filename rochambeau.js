3d864419ab20a3a524dff7164ace53af75743e1f
Kimberly C Reyes <kr61336@srcs.loc>
2018-05-30 12:19:40 -0700

    process

--- rochambeau.js
+++ rochambeau.js
@@ -58,3 +58,44 @@ rockButton.addEventListener('click', () => {storePlayerChoice(0)});
 paperButton.addEventListener('click', () => {storePlayerChoice(1)});
 scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
 playButton.addEventListener('click', () => {playGame()});
+//Displays the result of the game
+function displayGameResult(result){
+    // Create a message for the player
+    var message = "Your choice was " + playerChoice + " and the computer's choice was " + computerChoice + ".";
+    // Add to the base message if it was a win, loss, or tie
+    if (result === "win") {
+        // Display that it was a win
+        document.getElementById("result").textContent = message + " YOU WIN!";
+        document.getElementById("result").className = "alert alert-success";
+    } else if (result === "lose") {
+        // Display that it was a loss
+        document.getElementById("result").textContent = message + " YOU LOSE!";
+        document.getElementById("result").className = "alert alert-danger";
+    } else {
+        // Display that it was a tie
+        document.getElementById("result").textContent = message + " A tie.";
+        document.getElementById("result").className = "alert alert-info";
+    }
+}
+// This is the function for playing the game
+function playGame(){
+    // Here is the game ruleset algorithm
+    if (playerChoice == computerChoice) {
+        // We have a tie!
+        displayGameResult("tie")
+    } else if (playerChoice == 0 && computerChoice == 2) {
+        // Rock beats scissors - a win!
+        displayGameResult("win")
+    } else if (playerChoice == 1 && computerChoice == 0) {
+        // Paper beats scissors - a win!
+        displayGameResult("win")
+    } else if (playerChoice == 2 && computerChoice == 1) {
+        // Scissors beats paper - a win!
+        displayGameResult("win")
+    } else {
+        // All other combinations are losses
+        displayGameResult("lose")
+    }
+}
+// Stores the lables for the choices
+var choices = ["Rock", "Paper", "Scissors"];
